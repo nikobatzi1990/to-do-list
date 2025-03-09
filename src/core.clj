95d194c67901f6587c934c57f450d225faf6ea4d
nikobatzi1990 <nicole.boci@gmail.com>
2025-03-08 23:27:12 -0500

    wip: build sql database

---
+++ src/core.clj
@@ -0,0 +1,17 @@
+(ns core
+  (:require [next.jdbc :as jdbc]))
+  
+  (def db {:dbtype "h2" :dbname "to-do-list"})
+  (def ds (jdbc/get-datasource db))
+  
+  (jdbc/execute! ds ["
+  create table tasks (
+    id int auto_increment primary key,
+    task varchar(255)
+  )"])
+  
+  (jdbc/execute! ds ["
+  insert into tasks(task)
+    values('do laundry')"])
+  
+  (jdbc/execute! ds ["select * from tasks"])
--- src/hello.clj
+++
@@ -1,6 +0,0 @@
-(ns hello)
-
-(defn hello [who]
-  (str "Hello," who))
-
-(hello " Nicole")
