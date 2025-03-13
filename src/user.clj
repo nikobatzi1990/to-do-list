8ff7b462f6abafbd0793df5e1379d325e21b072e
nikobatzi1990 <nicole.boci@gmail.com>
2025-03-13 00:12:30 -0400

    feat: migration functions

--- src/user.clj
+++ src/user.clj
@@ -1,5 +1,24 @@
 (ns user
   (:require [migratus.core] :as migratus))
 
+(def config {:store :database
+             :migration-dir "resources/migrations"
+             :db {:dbtype "h2"
+                  :dbname "to-do-list"
+                  :host "localhost"
+                  :user "username"
+                  :password "password"}})
+
+(migratus/init config)
+
 (defn migrate []
-  (migratus/migrate ()))
+  (migratus/migrate config))
+
+(defn create-migration [name]
+  (migratus.core/create config name))
+
+(defn rollback-migration []
+  (migratus/rollback config))
+
+(migrate)
+(create-migration "create-database")
